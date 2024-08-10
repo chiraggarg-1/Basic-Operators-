cdbb3abbf9ea7ac6a2fe138b07e191f20872f8b5
CHIRAGGARG1697 <chiraggarg.0106@gmail.com>
2024-08-10 18:51:50 +0530

    First Commit

---
+++ Operators.py
@@ -0,0 +1,20 @@
+number = 1+2*3/4.0
+print(number)
+remainder = 11%3
+print(remainder)
+squared = 7**2
+cube = 2 ** 3
+print(squared)
+print(cube)
+helloworld = 'hello'+' '+'world'
+print(helloworld)
+lotsofhellos = ' hellos' * 10
+print(lotsofhellos)
+even_numbers = [2,4,6,8,10]
+odd_numbers = [1,3,5,7,9]
+all_numbers = even_numbers+odd_numbers
+print(all_numbers)
+a= [1,2,3]*4
+print(a)
+
+
